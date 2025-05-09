f14ff6f0c4106f86302346430bd49dccc7205261
ShenSingh <gayashan9090@gmail.com>
2025-05-10 05:04:14 +0530

    Done Main Func

---
+++ screpts/global.gd
@@ -0,0 +1,25 @@
+extends Node
+
+var player_current_attack = false
+
+var current_scene = "world"
+var transition_scene = false
+
+var player_exit_cliffside_posx = 200
+var player_exit_cliffside_posy = 29
+
+var player_start_posx = 155
+var player_start_posy = 108
+
+var game_first_loading = true
+
+func finish_changescenes():
+	
+	if transition_scene == true:
+		transition_scene = false
+		
+		if current_scene == "world":
+			current_scene = "cliff_side"
+			
+		else:
+			current_scene = "world"
